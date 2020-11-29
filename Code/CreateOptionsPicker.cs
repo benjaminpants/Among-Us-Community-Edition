510fc0561b24bf82ec46f89a2afa59a64c117080
CarJem Generations <Cwallacecs@Gmail.com>
2020-11-29 17:17:25 -0500

    Changes based on V4 Hotfix. (Un-implementedCode is in the Code Folder)

---
+++ Code/CreateOptionsPicker.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using InnerNet;
+using UnityEngine;
+
+// Token: 0x020001BA RID: 442
+public class CreateOptionsPicker : MonoBehaviour
+{
+	// Token: 0x0600098C RID: 2444 RVA: 0x00031C14 File Offset: 0x0002FE14
+	public void Start()
+	{
+		//this.MapButtons[1].gameObject.SetActive(TempData.IsDo2Enabled);
+		GameOptionsData targetOptions = this.GetTargetOptions();
+		this.UpdateImpostorsButtons(targetOptions.NumImpostors);
+		this.UpdateMaxPlayersButtons(targetOptions);
+		this.UpdateLanguageButtons(targetOptions.Keywords & GameKeywords.AllLanguages);
+		this.UpdateMapButtons((int)targetOptions.MapId);
+	}
+
+	// Token: 0x0600098D RID: 2445 RVA: 0x00031C70 File Offset: 0x0002FE70
+	private GameOptionsData GetTargetOptions()
+	{
+		if (this.mode == SettingsMode.Host)
+		{
+			return SaveManager.GameHostOptions;
+		}
+		GameOptionsData gameSearchOptions = SaveManager.GameSearchOptions;
+		if (gameSearchOptions.MapId == 0)
+		{
+			gameSearchOptions.ToggleMapFilter(0);
+			SaveManager.GameSearchOptions = gameSearchOptions;
+		}
+		return gameSearchOptions;
+	}
+
+	// Token: 0x0600098E RID: 2446 RVA: 0x00007CF9 File Offset: 0x00005EF9
+	private void SetTargetOptions(GameOptionsData data)
+	{
+		if (this.mode == SettingsMode.Host)
+		{
+			SaveManager.GameHostOptions = data;
+			return;
+		}
+		SaveManager.GameSearchOptions = data;
+	}
+
+	// Token: 0x0600098F RID: 2447 RVA: 0x00031CA8 File Offset: 0x0002FEA8
+	public void SetMaxPlayersButtons(int maxPlayers)
+	{
+		GameOptionsData targetOptions = this.GetTargetOptions();
+		bool addCount = false;
+
+
+		if (maxPlayers == 5) addCount = true;
+		else if (maxPlayers == 4) addCount = false;
+		else return;
+
+		if (maxPlayers < targetOptions.NumImpostors) return;
+		if (addCount && targetOptions.MaxPlayers + 1 > 20) return;
+		if (!addCount && targetOptions.MaxPlayers - 1 < 4) return;
+
+		targetOptions.MaxPlayers = (addCount ? targetOptions.MaxPlayers + 1 : targetOptions.MaxPlayers - 1);
+		this.SetTargetOptions(targetOptions);
+		if (DestroyableSingleton<FindAGameManager>.InstanceExists)
+		{
+			DestroyableSingleton<FindAGameManager>.Instance.ResetTimer();
+		}
+		this.UpdateMaxPlayersButtons(targetOptions);
+	}
+
+	// Token: 0x06000990 RID: 2448 RVA: 0x00031CF4 File Offset: 0x0002FEF4
+	public void SetImpostorButtons(int numImpostors)
+	{
+		GameOptionsData targetOptions = this.GetTargetOptions();
+		targetOptions.NumImpostors = numImpostors;
+		this.SetTargetOptions(targetOptions);
+		this.UpdateImpostorsButtons(numImpostors);
+		this.UpdateMaxPlayersButtons(targetOptions);
+	}
+
+	// Token: 0x06000991 RID: 2449 RVA: 0x00031D20 File Offset: 0x0002FF20
+	private void UpdateImpostorsButtons(int numImpostors)
+	{
+		for (int i = 0; i < this.ImpostorButtons.Length; i++)
+		{
+			SpriteRenderer spriteRenderer = this.ImpostorButtons[i];
+			spriteRenderer.enabled = (spriteRenderer.name == numImpostors.ToString());
+		}
+	}
+
+	// Token: 0x06000992 RID: 2450 RVA: 0x00031D60 File Offset: 0x0002FF60
+	public void SetMap(int mapid)
+	{
+		GameOptionsData targetOptions = this.GetTargetOptions();
+		if (this.mode == SettingsMode.Host)
+		{
+			targetOptions.MapId = (byte)mapid;
+		}
+		else
+		{
+			targetOptions.ToggleMapFilter((byte)mapid);
+		}
+		this.SetTargetOptions(targetOptions);
+		if (DestroyableSingleton<FindAGameManager>.InstanceExists)
+		{
+			DestroyableSingleton<FindAGameManager>.Instance.ResetTimer();
+		}
+		this.UpdateMapButtons(mapid);
+	}
+
+	// Token: 0x06000993 RID: 2451 RVA: 0x00031DB0 File Offset: 0x0002FFB0
+	private void UpdateMapButtons(int mapid)
+	{
+		if (this.mode == SettingsMode.Host)
+		{
+			if (this.CrewArea)
+			{
+				this.CrewArea.SetMap(mapid);
+			}
+			for (int i = 0; i < this.MapButtons.Length; i++)
+			{
+				SpriteRenderer spriteRenderer = this.MapButtons[i];
+				spriteRenderer.color = ((spriteRenderer.name == mapid.ToString()) ? Color.white : Palette.DisabledGrey);
+			}
+			return;
+		}
+		GameOptionsData targetOptions = this.GetTargetOptions();
+		for (int j = 0; j < this.MapButtons.Length; j++)
+		{
+			SpriteRenderer spriteRenderer2 = this.MapButtons[j];
+			spriteRenderer2.color = (targetOptions.FilterContainsMap(byte.Parse(spriteRenderer2.name)) ? Color.white : Palette.DisabledGrey);
+		}
+	}
+
+	// Token: 0x06000994 RID: 2452 RVA: 0x00031E68 File Offset: 0x00030068
+	public void SetLanguageFilter(int keyword)
+	{
+		GameOptionsData targetOptions = this.GetTargetOptions();
+		targetOptions.Keywords &= ~GameKeywords.AllLanguages;
+		targetOptions.Keywords |= (GameKeywords)keyword;
+		this.SetTargetOptions(targetOptions);
+		if (DestroyableSingleton<FindAGameManager>.InstanceExists)
+		{
+			DestroyableSingleton<FindAGameManager>.Instance.ResetTimer();
+		}
+		this.UpdateLanguageButtons((GameKeywords)keyword);
+	}
+
+	// Token: 0x06000995 RID: 2453 RVA: 0x00031EB8 File Offset: 0x000300B8
+	private void UpdateLanguageButtons(GameKeywords button)
+	{
+		for (int i = 0; i < this.LanguageButtons.Length; i++)
+		{
+			SpriteRenderer spriteRenderer = this.LanguageButtons[i];
+			spriteRenderer.enabled = (spriteRenderer.name == button.ToString());
+		}
+	}
+
+	// Token: 0x06000997 RID: 2455 RVA: 0x00031F00 File Offset: 0x00030100
+	private void UpdateMaxPlayersButtons(GameOptionsData opts)
+	{
+		if (this.CrewArea)
+		{
+			this.CrewArea.SetCrewSize(opts.MaxPlayers, opts.NumImpostors);
+		}
+		for (int i = 0; i < this.MaxPlayerButtons.Length; i++)
+		{
+			SpriteRenderer spriteRenderer = this.MaxPlayerButtons[i];
+			if (spriteRenderer.name == "5") spriteRenderer.GetComponentInChildren<TextRenderer>().Text = "+";
+			else if (spriteRenderer.name == "4") spriteRenderer.GetComponentInChildren<TextRenderer>().Text = "-";
+			else
+            {
+				spriteRenderer.GetComponentInChildren<TextRenderer>().Text = "";
+				spriteRenderer.enabled = false;
+				spriteRenderer.GetComponentInChildren<TextRenderer>().Color = Palette.DisabledGrey;
+			}
+
+		}
+	}
+
+	// Token: 0x0400091D RID: 2333
+	public SpriteRenderer[] MaxPlayerButtons;
+
+	// Token: 0x0400091E RID: 2334
+	public SpriteRenderer[] ImpostorButtons;
+
+	// Token: 0x0400091F RID: 2335
+	public SpriteRenderer[] LanguageButtons;
+
+	// Token: 0x04000920 RID: 2336
+	public SpriteRenderer[] MapButtons;
+
+	// Token: 0x04000921 RID: 2337
+	public SettingsMode mode;
+
+	// Token: 0x04000922 RID: 2338
+	public CrewVisualizer CrewArea;
+}
---
+++ Code/CustomHatDefinition.cs
@@ -0,0 +1,125 @@
+using System;
+
+// Token: 0x020002DD RID: 733
+public class CustomHatDefinition
+{
+	// Token: 0x1700021E RID: 542
+	// (get) Token: 0x06000F27 RID: 3879
+	// (set) Token: 0x06000F28 RID: 3880
+	public string ID { get; set; } = "NullHat";
+
+	// Token: 0x1700021F RID: 543
+	// (get) Token: 0x06000F29 RID: 3881
+	// (set) Token: 0x06000F2A RID: 3882
+	public string NormalImg { get; set; } = string.Empty;
+
+	// Token: 0x17000220 RID: 544
+	// (get) Token: 0x06000F2B RID: 3883
+	// (set) Token: 0x06000F2C RID: 3884
+	public string FloorImg { get; set; } = string.Empty;
+
+	// Token: 0x17000221 RID: 545
+	// (get) Token: 0x06000F2D RID: 3885
+	// (set) Token: 0x06000F2E RID: 3886
+	public bool inFront { get; set; }
+
+	// Token: 0x17000222 RID: 546
+	// (get) Token: 0x06000F2F RID: 3887
+	// (set) Token: 0x06000F30 RID: 3888
+	public float NormalPosX { get; set; }
+
+	// Token: 0x17000223 RID: 547
+	// (get) Token: 0x06000F31 RID: 3889
+	// (set) Token: 0x06000F32 RID: 3890
+	public float NormalPosY { get; set; }
+
+	// Token: 0x17000224 RID: 548
+	// (get) Token: 0x06000F33 RID: 3891
+	// (set) Token: 0x06000F34 RID: 3892
+	public float NormalWidth { get; set; }
+
+	// Token: 0x17000225 RID: 549
+	// (get) Token: 0x06000F35 RID: 3893
+	// (set) Token: 0x06000F36 RID: 3894
+	public float NormalHeight { get; set; }
+
+	// Token: 0x17000226 RID: 550
+	// (get) Token: 0x06000F37 RID: 3895
+	// (set) Token: 0x06000F38 RID: 3896
+	public float NormalPivotX { get; set; }
+
+	// Token: 0x17000227 RID: 551
+	// (get) Token: 0x06000F39 RID: 3897
+	// (set) Token: 0x06000F3A RID: 3898
+	public float NormalPivotY { get; set; }
+
+	// Token: 0x17000228 RID: 552
+	// (get) Token: 0x06000F3B RID: 3899
+	// (set) Token: 0x06000F3C RID: 3900
+	public float FloorPosX { get; set; }
+
+	// Token: 0x17000229 RID: 553
+	// (get) Token: 0x06000F3D RID: 3901
+	// (set) Token: 0x06000F3E RID: 3902
+	public float FloorPosY { get; set; }
+
+	// Token: 0x1700022A RID: 554
+	// (get) Token: 0x06000F3F RID: 3903
+	// (set) Token: 0x06000F40 RID: 3904
+	public float FloorWidth { get; set; }
+
+	// Token: 0x1700022B RID: 555
+	// (get) Token: 0x06000F41 RID: 3905
+	// (set) Token: 0x06000F42 RID: 3906
+	public float FloorHeight { get; set; }
+
+	// Token: 0x1700022C RID: 556
+	// (get) Token: 0x06000F43 RID: 3907
+	// (set) Token: 0x06000F44 RID: 3908
+	public float FloorPivotX { get; set; }
+
+	// Token: 0x1700022D RID: 557
+	// (get) Token: 0x06000F45 RID: 3909
+	// (set) Token: 0x06000F46 RID: 3910
+	public float FloorPivotY { get; set; }
+
+	// Token: 0x1700022E RID: 558
+	// (get) Token: 0x06000F48 RID: 3912
+	// (set) Token: 0x06000F49 RID: 3913
+	public bool UsePointFiltering { get; set; }
+
+	// Token: 0x1700023A RID: 570
+	// (get) Token: 0x06000F89 RID: 3977
+	// (set) Token: 0x06000F8A RID: 3978
+	public string PreviewImg { get; set; } = string.Empty;
+
+	// Token: 0x17000248 RID: 584
+	// (get) Token: 0x06000FA5 RID: 4005
+	// (set) Token: 0x06000FA6 RID: 4006
+	public float PreviewPosX { get; set; }
+
+	// Token: 0x17000249 RID: 585
+	// (get) Token: 0x06000FA7 RID: 4007
+	// (set) Token: 0x06000FA8 RID: 4008
+	public float PreviewPosY { get; set; }
+
+	// Token: 0x1700024A RID: 586
+	// (get) Token: 0x06000FA9 RID: 4009
+	// (set) Token: 0x06000FAA RID: 4010
+	public float PreviewWidth { get; set; }
+
+	// Token: 0x1700024B RID: 587
+	// (get) Token: 0x06000FAB RID: 4011
+	// (set) Token: 0x06000FAC RID: 4012
+	public float PreviewHeight { get; set; }
+
+	// Token: 0x1700024C RID: 588
+	// (get) Token: 0x06000FAD RID: 4013
+	// (set) Token: 0x06000FAE RID: 4014
+	public float PreviewPivotX { get; set; }
+
+	// Token: 0x1700024D RID: 589
+	// (get) Token: 0x06000FAF RID: 4015
+	// (set) Token: 0x06000FB0 RID: 4016
+	public float PreviewPivotY { get; set; }
+}
