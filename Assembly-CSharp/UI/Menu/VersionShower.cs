3b0d55adfc6b46692122d7d6554d5da2f022e924
blaidev <85421187+blaidev@users.noreply.github.com>
2021-11-20 16:07:41 -0500

    sda

--- Assembly-CSharp/UI/Menu/VersionShower.cs
+++ Assembly-CSharp/UI/Menu/VersionShower.cs
@@ -158,7 +158,7 @@ public class VersionShower : MonoBehaviour
 	public void Update()
     {
 		//Debug.Log(ServerManager.Instance.availableServers.Length);
-		text.Text = "v0.5.17.4 - [r0]A bit bigger update[]" + "\nBuild ID:[r1]" + BuildID + "[]\nLua ID:[r1]" + LuaID + "[]\nHats ID:[r1]" + HatID + "[]\nColors ID:[r1]" + ColorID;
+		text.Text = "v0.5.17.4 - [r0]A bit bigger update![]" + "\nBuild ID:[r1]" + BuildID + "[]\nLua ID:[r1]" + LuaID + "[]\nHats ID:[r1]" + HatID + "[]\nColors ID:[r1]" + ColorID;
 		if (CE_LuaLoader.TheOmegaHash != lastlua)
         {
 			lastlua = CE_LuaLoader.TheOmegaHash;
