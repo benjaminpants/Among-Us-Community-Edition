c0febca46c7da39fd3b736d602d06a27130c7275
blaidev <85421187+blaidev@users.noreply.github.com>
2021-11-20 19:32:44 -0500

    25 players

--- Assembly-CSharp/UI/Menu/CreateOptionsPicker.cs
+++ Assembly-CSharp/UI/Menu/CreateOptionsPicker.cs
@@ -71,7 +71,7 @@ public class CreateOptionsPicker : MonoBehaviour
 		default:
 			return;
 		}
-		if (maxPlayers >= targetOptions.NumImpostors && (!flag || targetOptions.MaxPlayers + 1 <= 35) && (flag || targetOptions.MaxPlayers - 1 >= 4))
+		if (maxPlayers >= targetOptions.NumImpostors && (!flag || targetOptions.MaxPlayers + 1 <= 25) && (flag || targetOptions.MaxPlayers - 1 >= 4))
 		{
 			targetOptions.MaxPlayers = (flag ? (targetOptions.MaxPlayers + 1) : (targetOptions.MaxPlayers - 1));
 			SetTargetOptions(targetOptions);
