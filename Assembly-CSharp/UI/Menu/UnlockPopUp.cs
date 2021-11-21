a0f90cbcd8f073d6158a0de2ec1b6299ae01db57
blaidev <85421187+blaidev@users.noreply.github.com>
2021-11-21 14:25:01 -0500

    sus

--- Assembly-CSharp/UI/Menu/UnlockPopUp.cs
+++ Assembly-CSharp/UI/Menu/UnlockPopUp.cs
@@ -7,7 +7,16 @@ public class UnlockPopUp : MonoBehaviour
 	public IEnumerator Show()
 	{
 		DateTime utcNow = DateTime.UtcNow;
-		if ((utcNow.DayOfYear >= 350 || utcNow.DayOfYear <= 4) && !SaveManager.GetPurchase("hats_newyears2018"))
+		if ((utcNow.DayOfYear >= 350 || utcNow.DayOfYear <= 8) && !SaveManager.GetPurchase("hats_newyears2018"))
+		{
+			base.gameObject.SetActive(value: true);
+			SaveManager.SetPurchased("hats_newyears2018");
+			while (base.isActiveAndEnabled)
+			{
+				yield return null;
+			}
+			/*
+		if ((utcNow.DayOfYear >= 120 || utcNow.DayOfYear <= 8) && !SaveManager.GetPurchase("hats_newyears2018"))
 		{
 			base.gameObject.SetActive(value: true);
 			SaveManager.SetPurchased("hats_newyears2018");
@@ -16,5 +25,6 @@ public class UnlockPopUp : MonoBehaviour
 				yield return null;
 			}
 		}
+		/*
 	}
 }
