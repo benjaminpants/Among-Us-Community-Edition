b5fcf8386d843d259d79ddf9fcad04abedc502be
impostor <77080832+impostor5875@users.noreply.github.com>
2021-11-21 15:55:46 -0500

    fix

--- Assembly-CSharp/UI/Menu/StatsPopup.cs
+++ Assembly-CSharp/UI/Menu/StatsPopup.cs
@@ -25,7 +25,6 @@ public class StatsPopup : MonoBehaviour
         Strings.Add($"Games Finished:\t{StatsManager.Instance.GamesFinished}");
         Strings.Add($"Stalemates: \t\t{StatsManager.Instance.Stalemates}");
         Strings.Add($"Deaths: \t\t{StatsManager.Instance.TimesMurdered}");
-	Strings.Add($"Game Mode Starts: \t\t{StatsManager.Instance.gamemodestarts}");
         foreach (KeyValuePair<string, uint> kvp in StatsManager.Instance.GameStarts)
         {
             Strings.Add(kvp.Key + " Games: \t\t" + kvp.Value);
