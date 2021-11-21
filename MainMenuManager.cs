dfecca0494f3c6a5fcafea77e8ea13acdb6ed68d
blaidev <85421187+blaidev@users.noreply.github.com>
2021-11-21 11:26:34 -0500

    creditsbuttonnolongerunused

--- MainMenuManager.cs
+++ MainMenuManager.cs
@@ -120,12 +120,12 @@ public class MainMenuManager : MonoBehaviour
 			{
 				component.sprite = PlayerIcon;
 			}
-			/*
+			
 			if ((bool)component && component.name == "CreditsButton")
 			{
 				component.sprite = FoundersIcon;
 			}
-			*/
+			
 		}
 	}
 
