4d56319640b489c6d875c8436c436e4403d3bf29
dumbguythatsontheinternet <85421187+blaidev@users.noreply.github.com>
2021-11-30 15:12:10 -0500

    Update and rename CE_FakePlayerDumb.cs to CE_FakePlayer.cs

---
+++ Assembly-CSharp/Custom/Lua/Interfaces/CE_FakePlayer.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+public class CE_FakePlayer
+{
+    public byte ColorId = 0;
+    public byte HatId = 0;
+    public string Name = "Bob";
+
+    CE_FakePlayer()
+    {
+      
+    }
+
+    CE_FakePlayer(byte color, byte hat string name)
+    {
+        ColorId = color;
+        HatId = hat;
+        Name = name;
+    }
+}
