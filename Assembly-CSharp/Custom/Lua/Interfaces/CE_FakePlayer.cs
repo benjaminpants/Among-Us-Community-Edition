5ff57bd7241a0937b6d47beac3883497b63395a9
dumbguythatsontheinternet <85421187+blaidev@users.noreply.github.com>
2021-11-30 07:12:22 -0500

    Update CE_FakePlayer.cs

--- Assembly-CSharp/Custom/Lua/Interfaces/CE_FakePlayer.cs
+++ Assembly-CSharp/Custom/Lua/Interfaces/CE_FakePlayer.cs
@@ -6,16 +6,18 @@ using System.Threading.Tasks;
 public class CE_FakePlayer
 {
     public byte ColorId = 0;
+    public byte HatId = 0;
     public string Name = "Bob";
 
     CE_FakePlayer()
     {
-
+      
     }
 
-    CE_FakePlayer(byte color, string name)
+    CE_FakePlayer(byte color, byte hat string name)
     {
         ColorId = color;
+        HatId = hat;
         Name = name;
     }
 }
