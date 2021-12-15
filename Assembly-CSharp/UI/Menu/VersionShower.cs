60347d9e968e2da738e4edd30ba205d2774177c9
FurryColtin <95934806+FurryColtin@users.noreply.github.com>
2021-12-15 12:28:02 -0500

    Now it initizalizes custom maps

--- Assembly-CSharp/UI/Menu/VersionShower.cs
+++ Assembly-CSharp/UI/Menu/VersionShower.cs
@@ -6,8 +6,9 @@ public class VersionShower : MonoBehaviour
 
 	public void Start()
 	{
-		text.Text = "v0.1.0 - First Release DONT LEAK!!";
+		text.Text = "v0.1.0 - First Release DONT LEAK!";
 		Screen.sleepTimeout = -1;
+		CE_CustomMapManager.Initialize();
                 CE_Extensions.OnStartup();
 	}
 
