54a5c5d35dcba40f6d7c0cbb017381091af792bb
FurryColtin <95934806+FurryColtin@users.noreply.github.com>
2021-12-15 12:23:47 -0500

    Create CE_MapInfo.cs

---
+++ Assembly-CSharp/Custom/Mapping/CE_MapInfo.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class CE_MapInfo
+{
+    public string MapName;
+   // public string[] CustomLocationNames = new string[12];
+   // public bool IsCustom;
+    public string ContentFolder;
+    public CEM_Map Map;
+
+
+    public CE_MapInfo()
+    {
+        MapName = "Basic Info";
+   //     IsCustom = false;
+    }
+
+    public CE_MapInfo(string mapname)
+    {
+        MapName = mapname;
+      //  IsCustom = false;
+    }
+    public CE_MapInfo(string mapname)
+    {
+        MapName = mapname;
+      //  IsCustom = iscustom;
+    }
+
+    public CE_MapInfo(string mapname, CEM_Map map, string content_folder)
+    {
+        MapName = mapname;
+      //  IsCustom = true;
+        Map = map;
+        ContentFolder = content_folder;
+    }
+    public CE_MapInfo(string mapname)
+    {
+        MapName = mapname;
+       // IsCustom = true;
+      //  CustomLocationNames = customlocals;
+    }
+}
